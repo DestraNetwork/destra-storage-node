da437dcf4a2f5948364c7758712d2a4feda80f27
0xricknpc <oxricknpc@icloud.com>
2024-04-25 03:39:22 +0800

    beta base v0.0.1 - w/o incentive layer

---
+++ README.md
@@ -0,0 +1,86 @@
+# Destra Decentralized Storage - Storage Node (Beta)
+
+This project contains the codebase necessary to run a storage node for the Destra Decentralized Storage Network. It uses bootstrap nodes registered on DestraStorageBootstrapPeerRegistry contract for `libp2p` peer-to-peer network capabilities, `ethers` for Ethereum blockchain interactions, and several other libraries to support decentralized data storage architecture.
+
+## Prerequisites
+
+Before you start, you will need:
+
+- Node.js and npm installed. You can download these from [Node.js official website](https://nodejs.org/).
+- A public IP address where your node will be accessible.
+- A system running Ubuntu.
+- Sepolia JSON-RPC API.
+
+## Setup Instructions
+
+### Clone the repository
+
+First, clone this repository to your local machine using the following command:
+
+```
+git clone https://github.com/DestraNetwork/destra-storage-node
+cd destra-storage-node
+```
+
+### Install npm dependencies
+
+Run the following command to install the required npm dependencies:
+
+```
+npm install
+```
+
+### Set up environment variables
+
+You will need to set up environment variables by copying the `.env.example` with following command:
+
+```
+cp .env.example .env
+```
+
+Now, you need to fill the following env variables:
+
+
+```
+RPC_URL=<Your_Ethereum_Node_RPC_URL>
+PRIVATE_KEY=<Your_Private_Key>
+CONTRACT_ADDRESS=<Destra_Peer_Registry_Address> [Sepolia: 0xf0DB1777c6f5E7Afb6d9a5af095AE008B9B2aA98]
+PUBLIC_IP=<Your_Public_IP>
+NODE_PORT=<Port_for_Node_to_Listen_On> [Default for Destra Storage Node is 10806]
+BLOCKSTORE_DIRECTORY=<Path_For_Blockstore>
+SUBGRAPH_URL=<URL_For_Destra_Storage_Subgraph> [https://api.studio.thegraph.com/query/69390/destra-storage-bootstrap-nodes/version/latest]
+```
+
+Replace `<Your_Ethereum_Node_RPC_URL>`, `<Your_Private_Key>`, `<Destra_Peer_Registry_Address>`, `<Your_Public_IP>`, `<Port_for_Node_to_Listen_On>`, `<Path_For_Blockstore>`, and `URL_For_Destra_Storage_Subgraph>` with your actual values.
+
+### Configure the firewall
+
+To allow traffic to your node, you'll need to open the specified port in the firewall. On Ubuntu, you can do this with the following commands:
+
+```
+sudo ufw allow <Port_for_Node_to_Listen_On>/tcp
+```
+
+Replace `<Port_for_Node_to_Listen_On>` with the port number you are using.
+
+
+### Start the Node
+
+Now, you can start your storage node with the following command. We recommend using tmux or screen:
+
+```
+npm run destra-storage-node
+```
+
+This will initialize your storage node, connects it with the bootstrap nodes and joins the Destra Storage Network
+
+
+## Contributing
+
+Contributions are welcome! Please feel free to submit pull requests to the project.
+
+## Support
+
+For support, open an issue in the GitHub issue tracker for this repository.
+
+Thank you for participating in the Destra network!
---
+++ storage-node.js
@@ -0,0 +1,161 @@
+import 'dotenv/config'
+import { noise } from '@chainsafe/libp2p-noise'
+import { yamux } from '@chainsafe/libp2p-yamux'
+import { identify } from '@libp2p/identify'
+import { bootstrap } from '@libp2p/bootstrap'
+import { tcp } from '@libp2p/tcp'
+import { FsBlockstore } from 'blockstore-fs'
+import { MemoryDatastore } from 'datastore-core'
+import { createHelia } from 'helia'
+import { createLibp2p } from 'libp2p'
+import { kadDHT, removePrivateAddressesMapper } from '@libp2p/kad-dht'
+import fs from 'fs';
+import { ethers } from 'ethers';
+import contractABI from './abis/BootstrapPeerRegistry.json'  with { type: "json" };
+import net from 'net';
+
+
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
+const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const contractAddress = process.env.CONTRACT_ADDRESS;
+const contract = new ethers.Contract(contractAddress, contractABI.abi, wallet);
+
+
+async function fetchPeerIdsFromSubgraph() {
+    const query = `
+    query GetBootstrapNodes {
+      peerRegistereds(first: 8) {
+        peerId
+        locationMultiAddr
+      }
+    }
+  `;
+
+    const response = await fetch(process.env.SUBGRAPH_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query }),
+    });
+
+    const { data } = await response.json();
+    return data.peerRegistereds;
+}
+
+async function fetchBootstrapNodes(peerIds) {
+    const bootstrapNodes = [];
+
+    for (let peer of peerIds) {
+        const nodeDetails = await contract.bootstrapNodes(peer.peerId);
+        bootstrapNodes.push(nodeDetails.locationMultiAddr + "/p2p/" + peer.peerId);
+    }
+
+    return bootstrapNodes;
+}
+
+function pingHost(ip, port, timeout = 10000) {
+    return new Promise((resolve, reject) => {
+        const socket = new net.Socket();
+        const address = `${ip}:${port}`;
+
+        socket.setTimeout(timeout);
+        socket.on('connect', () => {
+            console.log(`Successfully connected to ${address}`);
+            socket.destroy();
+            resolve(true);
+        });
+
+        socket.on('timeout', () => {
+            console.log(`Connection to ${address} timed out.`);
+            socket.destroy();
+            reject(new Error('Connection timed out'));
+        });
+
+        socket.on('error', (err) => {
+            console.log(`Connection to ${address} failed: ${err.message}`);
+            socket.destroy();
+            reject(err);
+        });
+
+        socket.connect(port, ip);
+    });
+}
+
+
+async function createDestraStorageNode() {
+    console.log("Creating Destra Storage Node...");
+
+    const blockstore = new FsBlockstore('./block_store')
+
+    const datastore = new MemoryDatastore()
+
+
+    console.log("Configuring libp2p...");
+
+    const peerIds = await fetchPeerIdsFromSubgraph();
+    const bootstrapNodes = await fetchBootstrapNodes(peerIds);
+    console.log("Fetched the bootstrap nodes from DestraStorageBootstrapPeerRegistry contract at", process.env.CONTRACT_ADDRESS, ":", bootstrapNodes)
+
+    const libp2p = await createLibp2p({
+        datastore,
+        addresses: {
+            listen: [
+                `/ip4/0.0.0.0/tcp/${process.env.NODE_PORT}`
+            ]
+        },
+        transports: [
+            tcp()
+        ],
+        connectionEncryption: [
+            noise()
+        ],
+        streamMuxers: [
+            yamux()
+        ],
+        peerDiscovery: [
+            bootstrap({
+                list: bootstrapNodes
+            })
+        ],
+        services: {
+            identify: identify(),
+            aminoDHT: kadDHT({
+                protocol: '/ipfs/kad/1.0.0',
+                peerInfoMapper: removePrivateAddressesMapper
+            })
+        }
+    })
+
+    console.log("libp2p configured. Initializing Destra Storage Node...");
+
+
+    return await createHelia({
+        datastore,
+        blockstore,
+        libp2p
+    })
+}
+
+
+
+async function main() {
+    const destraStorageNode = await createDestraStorageNode()
+    console.log("Destra Storage Node initialized successfully.");
+
+    const multiAddrLocation = `/ip4/${process.env.PUBLIC_IP}/tcp/${process.env.NODE_PORT}`
+    const peerId = destraStorageNode.libp2p.peerId.toString()
+
+    console.log("Destra Storage Node started with MultiAddress and Peer ID:", multiAddrLocation, peerId);
+
+    console.log(`Pinging ${process.env.PUBLIC_IP} on port ${process.env.NODE_PORT} to check firewall settings...`);
+    await pingHost(process.env.PUBLIC_IP, process.env.NODE_PORT);
+    console.log("Ping successful, your firewall settings appear to be correct.");
+
+
+
+    console.log("------------------------------------------------\n" +
+        "All set, your Destra Storage Node is up and running");
+}
+
+main();
