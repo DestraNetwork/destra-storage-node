59221f967e76fc30030986fd89933c834fce5bec
0xricknpc <oxricknpc@icloud.com>
2024-07-17 02:33:22 +0530

    feat: nodes registration seperation and optimizations

--- storage-node.js
+++ storage-node.js
@@ -10,15 +10,18 @@ import { createHelia } from 'helia'
 import { createLibp2p } from 'libp2p'
 import { kadDHT, removePrivateAddressesMapper } from '@libp2p/kad-dht'
 import { ethers } from 'ethers';
-import contractABI from './abis/BootstrapPeerRegistry.json'  with { type: "json" };
+import bootstrapContractABI from './abis/BootstrapPeerRegistry.json'  with { type: "json" };
+import storageNodeContractABI from './abis/DestraStorageNodeRegistry.json'  with { type: "json" };
 import net from 'net';
-
 
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const contractAddress = process.env.CONTRACT_ADDRESS;
-const contract = new ethers.Contract(contractAddress, contractABI.abi, wallet);
 
+const bootstrapContractAddress = process.env.BOOTSTRAP_CONTRACT_ADDRESS;
+const bootstrapContract = new ethers.Contract(bootstrapContractAddress, bootstrapContractABI.abi, wallet);
+
+const storageNodeContractAddress = process.env.STORAGE_NODE_CONTRACT_ADDRESS;
+const storageNodeContract = new ethers.Contract(storageNodeContractAddress, storageNodeContractABI.abi, wallet);
 
 async function fetchPeerIdsFromSubgraph() {
     const query = `
@@ -46,7 +49,7 @@ async function fetchBootstrapNodes(peerIds) {
     const bootstrapNodes = [];
 
     for (let peer of peerIds) {
-        const nodeDetails = await contract.bootstrapNodes(peer.peerId);
+        const nodeDetails = await bootstrapContract.bootstrapNodes(peer.peerId);
         bootstrapNodes.push(nodeDetails.locationMultiAddr + "/p2p/" + peer.peerId);
     }
 
@@ -81,21 +84,18 @@ function pingHost(ip, port, timeout = 10000) {
     });
 }
 
-
 async function createDestraStorageNode() {
     console.log("Creating Destra Storage Node...");
 
-    console.log("Setting up directory for block store...")
-    const blockstore = new FsBlockstore(process.env.BLOCKSTORE_DIRECTORY)
-
-    const datastore = new MemoryDatastore()
-
+    console.log("Setting up directory for block store...");
+    const blockstore = new FsBlockstore(process.env.BLOCKSTORE_DIRECTORY);
+    const datastore = new MemoryDatastore();
 
     console.log("Configuring libp2p...");
 
     const peerIds = await fetchPeerIdsFromSubgraph();
     const bootstrapNodes = await fetchBootstrapNodes(peerIds);
-    console.log("Fetched the bootstrap nodes from DestraStorageBootstrapPeerRegistry contract at", process.env.CONTRACT_ADDRESS, ":", bootstrapNodes)
+    console.log("Fetched the bootstrap nodes from BootstrapPeerRegistry contract at", bootstrapContractAddress, ":", bootstrapNodes);
 
     const libp2p = await createLibp2p({
         datastore,
@@ -125,31 +125,52 @@ async function createDestraStorageNode() {
                 peerInfoMapper: removePrivateAddressesMapper
             })
         }
-    })
+    });
 
     console.log("libp2p configured. Initializing Destra Storage Node...");
-
 
     return await createHelia({
         datastore,
         blockstore,
         libp2p
-    })
+    });
 }
 
+async function registerNode(peerId, locationMultiAddr) {
+    try {
+        const tx = await storageNodeContract.registerNode(peerId, locationMultiAddr);
+        await tx.wait();
+        console.log(`Node registered with peer ID ${peerId}`);
+    } catch (error) {
+        console.error(`Failed to register node with peer ID ${peerId}: ${error.message}`);
+    }
+}
 
+async function recordCheckpoint(peerId) {
+    try {
+        const tx = await storageNodeContract.recordCheckpoint(peerId);
+        await tx.wait();
+        console.log(`Checkpoint recorded for peer ID ${peerId}`);
+    } catch (error) {
+        console.error(`Failed to record checkpoint for peer ID ${peerId}: ${error.message}`);
+    }
+}
 
 async function main() {
-    const destraStorageNode = await createDestraStorageNode()
+    const destraStorageNode = await createDestraStorageNode();
     console.log("Destra Storage Node initialized successfully.");
 
-    const multiAddrLocation = `/ip4/${process.env.PUBLIC_IP}/tcp/${process.env.NODE_PORT}`
-    const peerId = destraStorageNode.libp2p.peerId.toString()
+    const multiAddrLocation = `/ip4/${process.env.PUBLIC_IP}/tcp/${process.env.NODE_PORT}`;
+    const peerId = destraStorageNode.libp2p.peerId.toString();
 
     console.log("Destra Storage Node started with MultiAddress and Peer ID:", multiAddrLocation, peerId);
 
+    console.log("Registering node with Destra Storage Nodes Registry...")
+    await registerNode(peerId, multiAddrLocation);
 
-
+    setInterval(async () => {
+        await recordCheckpoint(peerId);
+    }, 60 * 60 * 1000); 
 
     console.log("------------------------------------------------\n" +
         "All set, your Destra Storage Node is up and running");
